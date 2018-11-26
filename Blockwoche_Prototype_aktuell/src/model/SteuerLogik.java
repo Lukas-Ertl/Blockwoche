1fb0259adaf2e93ba9a88eeb41e59553a45468d4
Personface <Personface@DESKTOP-SNSID7H>
2018-11-26 16:41:01 +0100

    full compliance (hopefully) and some comments

--- Blockwoche_Prototype_aktuell/src/model/SteuerLogik.java
+++ Blockwoche_Prototype_aktuell/src/model/SteuerLogik.java
@@ -1,23 +1,42 @@
 package model;
 
-public class SteuerLogik {
-	private long rotPhase;
+public class SteuerLogik extends Actor
+{
+	//time that an Ampel stays green
 	private long gruenPhase;
+	//time that an Ampel stays red
+	private long rotPhase;
+	//TODO replace with a list of Ampeln
 	private Ampel myAmpel;
+	//time before the next wave of cars start TODO change to list
 	private long wellenZeitPunkt;
+	//TODO replace with list of WellenGenerators
 	private WellenGenerator myWellenGenerator;
 	
-	private void work()
+	protected SteuerLogik(String label, int xPos, int yPos) {
+		super(label, xPos, yPos);
+		
+	}
+
+	protected boolean work()
+	{
+		
+		return false;
+	}
+	
+	//change state of Ampeln (form Green to Red, and from Red to Green)
+	private void updateAmpeln()
 	{
 		
 	}
 	
-	private void update()
+	//send WellenGenerator a notice that it should send cars
+	private void updateWellenGenerator()
 	{
 		
 	}
 	
-	//temp class to make this FUCKING RED LINE DISAPPEAR
+	//temp class
 	private class WellenGenerator
 	{
 		public void sendWave() {};
