62dd12e5390a5f1992b628afc55200492f3155c7
Personface <Personface@DESKTOP-SNSID7H>
2018-11-26 16:33:50 +0100

    Add basic template for SteuerLogik

---
+++ Blockwoche_Prototype_aktuell/src/model/SteuerLogik.java
@@ -0,0 +1,25 @@
+package model;
+
+public class SteuerLogik {
+	private long rotPhase;
+	private long gruenPhase;
+	private Ampel myAmpel;
+	private long wellenZeitPunkt;
+	private WellenGenerator myWellenGenerator;
+	
+	private void work()
+	{
+		
+	}
+	
+	private void update()
+	{
+		
+	}
+	
+	//temp class to make this FUCKING RED LINE DISAPPEAR
+	private class WellenGenerator
+	{
+		public void sendWave() {};
+	}
+}
