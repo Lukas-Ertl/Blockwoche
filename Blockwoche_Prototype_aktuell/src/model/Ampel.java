d6c1e337347773c692376e88a1df4ed191d93404
Philippjaquart <Philippjaquart@141.7.159.56>
2018-11-27 16:41:57 +0100

    New Ampel

--- Blockwoche_Prototype_aktuell/src/model/Ampel.java
+++ Blockwoche_Prototype_aktuell/src/model/Ampel.java
@@ -5,21 +5,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * This is the station that will represent the traffic
- * lights themselves
+ * This is the Station that will represent the traffic
+ * lights themselves 
  * 
- * @author JMaier
+ * @author Team 4 
  * @version 2018-11-24
  */
 public class Ampel extends SimpleStation {
 	
+	 /** the boolean green is being used as the current state of the Ampel.
+	  * 
+	  */
 	private boolean green = true;
-	
-	
 	
 	protected static Map hm = Collections.synchronizedMap(new HashMap());
 
-
+/** 
+ * 
+ * 
+ * Constructor that constructs the initial states for the Ampel Objects 
+ * it puts the Ampel objects in a hashMap and gives them a keyvalue.
+ * @param label
+ * @param inQueue
+ * @param outQueue
+ * @param xPos
+ * @param yPos
+ * @param image
+ */
 	protected Ampel(String label, SynchronizedQueue inQueue, SynchronizedQueue outQueue, int xPos, int yPos,
 			String image) {
 		super(label, inQueue, outQueue, xPos, yPos, image);
@@ -28,12 +40,25 @@ public class Ampel extends SimpleStation {
 	}
 	
 
-
+	/**
+	 * this method handles the Cars by putting them in the outQueue
+	 * to simulate that the cars are driving.
+	 */
 	@Override
 	protected void handleObject(TheObject theObject) {
 		theObject.enterOutQueue(this);
 	}
 	
+	/**
+	 * Creates a new Ampel Object 
+	 * 
+	 * @param label
+	 * @param inQueue
+	 * @param outQueue
+	 * @param xPos
+	 * @param yPos
+	 * @param image
+	 */
 	public static void create(String label, SynchronizedQueue inQueue, SynchronizedQueue outQueue, int xPos, int yPos, String image){
 		
 		new Ampel(label, inQueue, outQueue, xPos, yPos, image);
@@ -42,7 +67,12 @@ public class Ampel extends SimpleStation {
 	
 	
 	
-	@Override
+	
+	/**
+	 *  returns a super call for the worker constructor of Simplestation 
+	 * and loops it while the Ampel is green.
+	 */
+	@Override 
 	protected boolean work() {
 		while(green) {
 			return super.work();
@@ -71,34 +101,44 @@ public class Ampel extends SimpleStation {
 		return false;
 	}
 	/**
-	 * Checks if the Lights are green then returns true for green and returns red when false
-	 * @return
+	 * Everytime switchstate is being called the boolean green is being inverted.
+	 * 
 	 */
-	
-	
 	public void switchState()
 	
 	{
 		green=!green;
 	
 	}
+	/**
+	 * getter method to return the Ampel keyValues in the hashMap
+	 * @param label
+	 * @return
+	 */
 	public static Ampel getAmpelByLabel(String label)
 	{
 		return (Ampel) hm.get(label);
 		
 	}
 	@Override
+	/**
+	 * Should handle Multiple Objects in the future
+	 */
 	protected void handleObjects(Collection<TheObject> theObjects)
 	{
 		
 	}
-	
+	/**
+	 * placeholder should return multiple Objects in the future
+	 */
 	@Override
 	protected Collection<TheObject> getNextInQueueObjects()
 	{
 		return null;
 	}
-
+	/**
+	 * placeholder should return muliple objects in the future
+ 	*/
 	@Override
 	protected Collection<TheObject> getNextOutQueueObjects()
 	{
