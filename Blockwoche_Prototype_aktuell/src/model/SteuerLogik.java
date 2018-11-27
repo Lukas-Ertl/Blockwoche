877ae9fe24a661091203902d57b418df17a665cf
Personface <Personface@DESKTOP-SNSID7H>
2018-11-27 08:41:25 +0100

    Hopefully working code

--- Blockwoche_Prototype_aktuell/src/model/SteuerLogik.java
+++ Blockwoche_Prototype_aktuell/src/model/SteuerLogik.java
@@ -1,5 +1,7 @@
 package model;
 
+import controller.Simulation;
+
 public class SteuerLogik extends Actor
 {
 	//time that an Ampel stays green
@@ -13,39 +15,62 @@ public class SteuerLogik extends Actor
 	//TODO replace with list of WellenGenerators
 	private WellenGenerator myWellenGenerator;
 	
-	protected SteuerLogik(String label, int xPos, int yPos, long rotphase, long gruenphase, String ampel, long wellenzeitpunkt, String wellengenerator)
+	//the time being waited for to send signals to Ampeln and WellenGeneratorn
+	private long ampelWaitTime, wellenGeneratorWaitTime;
+	//boolean for deciding between rotPhase and gruenPhase time
+	private boolean isGruen = true;
+	
+	protected SteuerLogik(String label, int xPos, int yPos, long rotPhase, long gruenPhase, Ampel ampel, long wellenZeitPunkt, WellenGenerator wellenGenerator)
 	{
 		super(label, xPos, yPos);
+		this.myAmpel = ampel;
+		this.rotPhase = rotPhase;
+		this.gruenPhase = gruenPhase;
+		this.wellenZeitPunkt = wellenZeitPunkt;
+		this.myWellenGenerator = wellenGenerator;
 		
+		this.ampelWaitTime = this.gruenPhase;
+		this.wellenGeneratorWaitTime = this.wellenZeitPunkt;
 	}
 	
-	public static void create(String label, int xPos, int yPos, long rotphase, long gruenphase, String ampel, long wellenzeitpunkt, String wellengenerator)
+	public static void create(String label, int xPos, int yPos, long rotPhase, long gruenPhase, String ampel, long wellenZeitPunkt, String wellenGenerator)
 	{
-		
+		Ampel a = Ampel.getAmpel(ampel);
+		WellenGenerator w = WellenGenerator.getWellenGeneratorByLabel(wellenGenerator);
+		new SteuerLogik(label, xPos, yPos, rotPhase, gruenPhase, ampel, wellenZeitPunkt, wellenGenerator);
 	}
 	
 
 	protected boolean work()
 	{
+		long simTime = Simulation.getGlobalTime();
 		
+		if(simTime > ampelWaitTime)
+		{
+			updateAmpeln(this.myAmpel);
+			
+			if(isGruen)
+				this.ampelWaitTime = Simulation.getGlobalTime() + rotPhase;
+			else
+				this.ampelWaitTime = Simulation.getGlobalTime() + gruenPhase;
+		}
+		if(simTime > wellenGeneratorWaitTime)
+		{
+			updateWellenGenerator(this.myWellenGenerator);
+			this.wellenGeneratorWaitTime = Simulation.getGlobalTime() + this.wellenZeitPunkt;
+		}
 		return false;
 	}
 	
 	//change state of Ampeln (form Green to Red, and from Red to Green)
-	private void updateAmpeln()
+	private void updateAmpeln(Ampel a)
 	{
-		
+		a.switchState();
 	}
 	
 	//send WellenGenerator a notice that it should send cars
-	private void updateWellenGenerator()
+	private void updateWellenGenerator(WellenGenerator w)
 	{
-		
-	}
-	
-	//temp class
-	private class WellenGenerator
-	{
-		public void sendWave() {};
+		w.sendWave();
 	}
 }
