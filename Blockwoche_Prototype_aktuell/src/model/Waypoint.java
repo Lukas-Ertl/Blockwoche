78b582abea7e682b1b28a03b709a71ea6307bfd6
Philippjaquart <Philippjaquart@141.7.159.56>
2018-11-29 16:20:44 +0100

    Waypoint done

--- Blockwoche_Prototype_aktuell/src/model/Waypoint.java
+++ Blockwoche_Prototype_aktuell/src/model/Waypoint.java
@@ -3,36 +3,48 @@ package model;
 import java.util.Collection;
 
 public class Waypoint extends SimpleStation {
-	
+
 	public Waypoint(String label, SynchronizedQueue inQueue, SynchronizedQueue outQueue, int xPos, int yPos)
-	
+
 	{
-		super(label, inQueue, outQueue, yPos, yPos, label);
-		
+		super(label, inQueue, outQueue, xPos, yPos, "");
+
 	}
 
+	public static void create(String label, SynchronizedQueue inQueue, SynchronizedQueue outQueue, int xPos, int yPos) {
+		new Waypoint(label, inQueue, outQueue, xPos, yPos);
+	}
+
+	/**
+	 * the method handles the cars like the Traffic light by putting them in the outqueue.
+	 */
 	@Override
 	protected void handleObject(TheObject theObject) {
-		// TODO Auto-generated method stub
-		
+		theObject.enterOutQueue(this);
+
 	}
 
 	@Override
 	protected void handleObjects(Collection<TheObject> theObjects) {
-		// TODO Auto-generated method stub
 		
+
 	}
 
 	@Override
 	protected Collection<TheObject> getNextInQueueObjects() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	protected Collection<TheObject> getNextOutQueueObjects() {
-		// TODO Auto-generated method stub
+		
 		return null;
+
+	}
+	protected boolean work() {
+		 
+			return super.work();
 	}
 
 }
